d57a46092ee5164113ca6b546cf0f251cf670165
Polvere243 <danielercoli@gmail.com>
2024-04-05 22:20:57 +0200

    used RouterVue instead of the pages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-import HomePage from './components/pages/HomePage.vue';
-import DetailPage from './components/pages/DetailPage.vue';
-import ContactUsPage from './components/pages/ContactUsPage.vue';
+
 export default {
   name: 'Glossario',
   components: {
-    AppHeader, HomePage, DetailPage, ContactUsPage
+    AppHeader
   }
 };
 </script>
