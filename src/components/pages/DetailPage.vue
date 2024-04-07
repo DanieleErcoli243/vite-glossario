1b211050e80dfb1ce89f15d5c5fa3d75a05464f9
Polvere243 <danielercoli@gmail.com>
2024-04-07 11:28:45 +0200

    imported card component and insert the content in data, but null

--- src/components/pages/DetailPage.vue
+++ src/components/pages/DetailPage.vue
@@ -1,10 +1,17 @@
 <script>
+import BaseCard from '../BaseCard.vue';
 import axios from 'axios';
 export default {
-    name: 'DetailPage'
+    name: 'DetailPage',
+    components: {
+        BaseCard
+    },
+    data: () => ({
+        word: null
+    })
 };
 </script>
 
 <template>
-    <h1>Pagina del dettaglio</h1>
+    <BaseCard />
 </template>
