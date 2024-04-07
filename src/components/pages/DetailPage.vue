e833ffc89e35a5df84ce83c144e963af87b34ff6
Polvere243 <danielercoli@gmail.com>
2024-04-07 11:42:22 +0200

    created a method to call at the created to get the data from the db to fill the card

--- src/components/pages/DetailPage.vue
+++ src/components/pages/DetailPage.vue
@@ -1,5 +1,6 @@
 <script>
 import BaseCard from '../BaseCard.vue';
+const baseUri = 'http://localhost:8000/api/words/';
 import axios from 'axios';
 export default {
     name: 'DetailPage',
@@ -8,7 +9,26 @@ export default {
     },
     data: () => ({
         word: null
-    })
+    }),
+    methods: {
+        getWord() {
+            try {
+                // raccolgo i dati dal database
+                const { data } = await axios.get(baseUri + this.$route.params.id);
+                // stampo i risultati in console
+                console.log(data);
+                // riassegno i dati al mio oggetto vuoto
+                this.word = data;
+            } catch (err) {
+                // segnalo un eventuale errore
+                console.error(err);
+            }
+
+        }
+    },
+    created() {
+        this.getWord();
+    }
 };
 </script>
 
